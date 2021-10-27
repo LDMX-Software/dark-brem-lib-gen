8f57b5d4cea8af89c6171fe4119ab91abe5e1acf
tomeichlersmith <eichl008@umn.edu>
2021-10-27 10:15:12 -0500

    only bash check if interactive

--- env.sh
+++ env.sh
@@ -21,10 +21,12 @@
 # All of this setup requires us to be in a bash shell.
 #   We add this check to make sure the user is in a bash shell.
 ####################################################################################################
-if [[ "$0" != *"bash"* ]]; then
-  echo "[dbgen-env.sh] [ERROR] You aren't in a bash shell. You are in '$0'."
-  [[ "$SHELL" = *"bash"* ]] || echo "  You're default shell '$SHELL' isn't bash."
-  return 1
+if tty -s; then
+  if [[ "$0" != *"bash"* ]]; then
+    echo "[dbgen-env.sh] [ERROR] You aren't in a bash shell. You are in '$0'."
+    [[ "$SHELL" = *"bash"* ]] || echo "  You're default shell '$SHELL' isn't bash."
+    return 1
+  fi
 fi
 
 ####################################################################################################
