9cb4c2ab1e623907d99d99a9de1aac97657aca1c
Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>
2022-05-17 09:17:50 -0500

    copy over work from ldmx-sim-technique

--- analysis/dark_brem_lhe.py
+++ analysis/dark_brem_lhe.py
@@ -1,21 +1,15 @@
 """Read dark brem LHE files
-
 We use the pylhe Python module developed by scikit-hep
 to parse the LHE files here.
-
   https://github.com/scikit-hep/pylhe
-
 pylhe API Notes
 ---------------
 Currently, the head of the master branch has diverged
 from the last release pretty significantly, so make sure
 you are on the current release tag when browsing the GitHub.
-
 readLHE returns a generator for looping over the events.
-
 LHEEvent 
   'particles' attribute which has the list of particles.
-
 LHEParticle
   'e' Energy
   'px', 'py', 'pz' 3 Momentum 
@@ -25,48 +19,14 @@ LHEParticle
 
 import pylhe
 import numpy
+import pandas
 import os
-
-class ColumnList(list) :
-    """Wrapper around a list of objects
-    so we can grab lists of those member variables.
-
-    We assume that the member variable requested from
-    each of the objects is a low-level number (e.g. float)
-    so that we can wrap the values in a numpy array.
-    """
-
-    def __getattr__(self, name) :
-        """Called after everything else,
-        we assume we are trying to get all entries for a specific
-        attribute of the entries in this list.
-
-        Fundamental types don't have the '__dict__' attribute,
-        so if the list is empty or the first entry has the '__dict__'
-        attribute, we return a ColumnList of the requested attribute.
-        Otherwise, we return a numpy.array of the requested attribute
-        (i.e. assuming that it is the last request).
-        """
-        raw_list = [getattr(o,name) for o in self]
-
-        if len(raw_list) == 0 :
-            return raw_list
-
-        if isinstance(raw_list[0], ColumnList) :
-            return ColumnList([item for sublist in raw_list for item in sublist])
-
-        if hasattr(raw_list[0],'__dict__') :
-            return ColumnList(raw_list)
-
-        return numpy.array(raw_list)
 
 class DarkBremEvent :
     """A Dark Brem event parsed from the LHE file
-
     This is an incredibly simple class which handles
     the assignment of physicist-helpful names to the particles
     in the LHE event.
-
     Attributes
     ----------
     dark_photon : pylhe.LHEParticle
@@ -92,7 +52,6 @@ class DarkBremEvent :
 
 def read_dark_brem_lhe(lhe_file) :
     """Generator of DarkBremEvents from the input LHE file
-
     This simply wraps the pylhe.readLHE python generator
     by wrapping their output event with our own event.
     """
@@ -102,7 +61,6 @@ def read_dark_brem_lhe(lhe_file) :
 
 class DarkBremEventFile :
     """In-memory storage of dark brem event kinematics for the input file
-
     After reading in some initialization parameters,
     we read in **all** of the events in the file.
     
@@ -117,12 +75,8 @@ class DarkBremEventFile :
         WARNING: This may not change if we are only changing the target mass for different target materials
     target_mass : float
         Mass of target nucleus [GeV]
-    recoil_lepton : pylhe.LHEParticle
-        The outgoing lepton
-    dark_photon : pylhe.LHEParticle
-        The outgoing dark photon
-    events : ColumnList
-        List of DarkBremEvents in this file, wrapped with column list to make retrieval of data easier
+    events : pandas.DataFrame
+        DataFrame of events in this file
     """
 
     def __init__(self, lhe_file) :
@@ -131,16 +85,42 @@ class DarkBremEventFile :
         self.incident_energy = self.full_init_info['initInfo']['energyA']
         self.target = int(self.full_init_info['initInfo']['beamB'])
         self.target_mass = self.full_init_info['initInfo']['energyB']
-        self.events = ColumnList(read_dark_brem_lhe(lhe_file))
+        event_data = []
+        for e in read_dark_brem_lhe(lhe_file) :
+            event_data.append({
+                'x' : 0.,
+                'y' : 0.,
+                'z' : 0.,
+                'incident_pdg' : e.incident_lepton.id,
+                'incident_genstatus' : e.incident_lepton.status,
+                'incident_mass' : e.incident_lepton.m,
+                'incident_energy' : e.incident_lepton.e,
+                'incident_px' : e.incident_lepton.px,
+                'incident_py' : e.incident_lepton.py,
+                'incident_pz' : e.incident_lepton.pz,
+                'recoil_pdg' : e.recoil_lepton.id,
+                'recoil_genstatus' : e.recoil_lepton.status,
+                'recoil_mass' : e.recoil_lepton.m,
+                'recoil_energy' : e.recoil_lepton.e,
+                'recoil_px' : e.recoil_lepton.px,
+                'recoil_py' : e.recoil_lepton.py,
+                'recoil_pz' : e.recoil_lepton.pz,
+                'aprime_pdg' : e.dark_photon.id,
+                'aprime_genstatus' : e.dark_photon.status,
+                'aprime_mass' : e.dark_photon.m,
+                'aprime_energy' : e.dark_photon.e,
+                'aprime_px' : e.dark_photon.px,
+                'aprime_py' : e.dark_photon.py,
+                'aprime_pz' : e.dark_photon.pz,
+            })
+        self.events = pandas.DataFrame(event_data)
 
     def __repr__(self) :
         return f'DarkBremEventFile(lepton=[{self.lepton},{self.incident_energy}GeV],target=[{self.target},{self.target_mass}GeV])'
 
 class DarkBremEventLibrary :
     """In-memory storage of dark brem event kinematics for an event library
-
     We basically hold all of the DarkBremEventFiles in one place.
-
     Attributes
     ----------
     lepton : int
@@ -152,12 +132,12 @@ class DarkBremEventLibrary :
         WARNING: This may not change if we are only changing the target mass for different target materials
     target_mass : float
         Mass of target nucleus [GeV]
-    files : ColumnList
-        List of DarkBremEventFiles in this library, wrapped with column list to make retrieval of data easier
+    files : list
+        List of DarkBremEventFiles in this library
     """
 
     def __init__(self, library_d) :
-        self.files = ColumnList([DarkBremEventFile(os.path.join(library_d,f)) for f in os.listdir(library_d) if f.endswith('lhe')])
+        self.files = [DarkBremEventFile(os.path.join(library_d,f)) for f in os.listdir(library_d) if f.endswith('lhe')]
         if len(self.files) == 0 :
             raise Exception(f'Passed library {library_d} does not have any LHE files in it.')
 
@@ -186,45 +166,7 @@ class DarkBremEventLibrary :
 
     def __str__(self) :
         return f'Dark Brem Event Library of {self.lepton_str} on {self.target_mass} GeV Target'
-
-def pt(particles) :
-    """Calculate transverse momentum for the input list of particles
     
-    Parameters
-    ----------
-    particles : ColumnList[pylhe.LHEParticle]
-        particles to calculate
-
-    Return
-    ------
-      numpy.array of pt values
-    """
-    return numpy.sqrt(particles.px**2 + particles.py**2)
-
-def cosphi(particles) :
-    """Calculate cos(phi) for the input list of particles
-    
-    Parameters
-    ----------
-    particles : ColumnList[pylhe.LHEParticle]
-        particles to calculate
-
-    Return
-    ------
-      numpy.array of cos(phi) values
-    """
-    return particles.px / pt(particles)
-
-def costheta(particles) :
-    """Calculate cos(theta) for the input list of particles
-
-    Parameters
-    ----------
-    particles : ColumnList[pylhe.LHEParticle]
-        particlese to calculate cos(theta) for
-
-    Return
-    ------
-      numpy.array of cos(theta) values
-    """
-    return particles.pz / numpy.sqrt( particles.px ** 2 + particles.py ** 2 + particles.pz ** 2 )
+    def events(self) :
+        """Get all of the events in this library in a single dataframe"""
+        return pandas.concat([f.events for f in self.files])
